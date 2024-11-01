ce4e8ebbca4b782dc7648b7e8def07d84eb4a1fa
TioElvis <elvis.veramijares@gmail.com>
2024-11-01 13:26:38 +0100

    .

---
+++ src/db/prisma.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client/extension";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var cachePrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachePrisma) {
+    global.cachePrisma = new PrismaClient();
+  }
+
+  prisma = global.cachePrisma;
+}
+
+export const db = prisma;
